d8e8c619bc7927248c8445ad9b32879a514ec0ba
ared <chandyajin@gmail.com>
2024-07-05 18:40:32 +0530

    added +page.svelte

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    import MainContent from "../components/MainContent.svelte";
+    import Sidebar from "../components/Sidebar.svelte";
+</script>
+
+<svelte:head>
+    <title>Home - The Classic Chronicle</title>
+</svelte:head>
+
+<MainContent />
+<Sidebar />
