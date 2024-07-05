28f26ca8efd73de99d9f8cb5027baeb7d4f4b439
ared <chandyajin@gmail.com>
2024-07-05 18:35:17 +0530

    added header

---
+++ src/components/Header.svelte
@@ -0,0 +1,26 @@
+<script>
+    let title = "The Classic Chronicle";
+</script>
+
+<header>
+    <h1>{title}</h1>
+    <p>Timeless Web Design Since 1998</p>
+</header>
+
+<style>
+    header {
+        background-color: #1a1a1a;
+        color: #ffffff;
+        padding: 20px;
+        text-align: center;
+    }
+    h1 {
+        font-size: 2.5em;
+        margin: 0;
+        font-weight: normal;
+    }
+    p {
+        font-style: italic;
+        margin: 5px 0 0;
+    }
+</style>
