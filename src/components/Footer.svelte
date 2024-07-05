a8664e690b53b57a943b2a4771cf43b29b7e1ade
ared <chandyajin@gmail.com>
2024-07-05 18:37:40 +0530

    added Footer.svelte

---
+++ src/components/Footer.svelte
@@ -0,0 +1,13 @@
+<footer>
+    <p>&copy; 2024 The Classic Chronicle | Powered by SvelteKit</p>
+</footer>
+
+<style>
+    footer {
+        background-color: #1a1a1a;
+        color: #ffffff;
+        text-align: center;
+        padding: 10px;
+        font-size: 0.9em;
+    }
+</style>
