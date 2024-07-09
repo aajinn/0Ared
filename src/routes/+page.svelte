ccb2866aa0b601b5c5d3e70da904351968f2131d
ared <chandyajin@gmail.com>
2024-07-09 21:26:29 +0530

    added meme

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+    import Animation from "../components/Animation.svelte";
+    import Meme from "../components/Meme.svelte";
+</script>
+
+<svelte:head>
+    <title>ARed</title>
+</svelte:head>
+<main>
+    <h1> ARed </h1>
+    <p>I love code.</p>
+    <a href="https://github.com/ARed99/">github</a>
+    <Animation />
+    <Meme />
+</main>
+
+<style>
+    main {
+        padding: 2rem;
+    }
+</style>
