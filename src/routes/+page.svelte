b0108fde99c41d0821a9dc49fc7e477cc18a48f7
ared <chandyajin@gmail.com>
2024-07-22 23:17:27 +0530

    content change

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,16 +5,47 @@
     <title>ARed</title>
 </svelte:head>
 <main>
-    <h1> ARed </h1>
+    <h1> Ajin Varghese Chandy </h1>
     <p>I love code.</p>
-    <p><a href="https://github.com/ARed99/">github</a></p>
-    <h1> hey, ✌️ </h1>
-    <p> discord : .ajin_v </p>
-    <p>X : <a href="https://x.com/ChandyAjin/">@ChandyAjin</a></p>
+    <p>newbie.</p>
+    <p>I used <a href="https://svelte.dev/">svelte</a> to build this site.</p>
+    <ul>
+        <b>
+            <li><p>c/c++</p></li>
+            <li><p>javascript</p></li>
+        </b>
+    </ul>
+    <p
+        >You feel bad because you know what you’re supposed to do and you aren’t
+        doing it.
+    </p>
+    <nav>
+        <ul>
+            <li><p><a href="https://github.com/ARed99/">Github</a></p></li>
+            <li> <p><a href="https://x.com/ChandyAjin/">X(twitter)</a></p></li>
+            <li>
+                <p><a href="https://www.youtube.com/@ARed11">Youtube</a></p></li
+            >
+        </ul>
+    </nav>
 </main>
 
 <style>
     main {
         padding: 2rem;
+    }
+    nav {
+        width: 100%;
+    }
+    nav ul {
+        display: flex;
+        list-style: none;
+        justify-content: start;
+        margin: 0%;
+        padding: 0%;
+    }
+    nav ul li {
+        padding: 1rem;
+        padding-left: 0%;
     }
 </style>
