d98f6d11de5d650aa671a037f2a83606ff790a32
ARed99 <chandyajin@gmail.com>
2024-08-30 20:13:06 +0530

    change

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"1f5i39MXFPfJY4RrYCgbf70MAUItNHBJeuCLE6CutGg=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"MBSoLc7en9lZYiubwHdMRwu3rXWQPkXKedhmodK1Rlk=\"}"
