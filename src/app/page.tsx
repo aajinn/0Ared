06210bbae64535c5422f74a01160a94641454d9f
ARed99 <chandyajin@gmail.com>
2024-08-30 10:08:32 +0530

    imported description and recentactiveites component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
 import Header from "@/components/Header";
+import Description from "@/components/Description";
+import RecentActivities from "@/components/RecentActivities";
+
 export default function Home() {
   return (
     <>
       <Header />
+      <main className="container mx-auto px-4 py-8">
+        <Description />
+        <RecentActivities />
+      </main>
     </>
   );
 }
