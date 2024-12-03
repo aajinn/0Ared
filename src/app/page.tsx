8b3d2bdf78ed8ddbb59169e7b0025c6c11caa591
ARed99 <chandyajin@gmail.com>
2024-12-03 10:50:03 +0530

    removed matrix animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
-import Description from "../components/Description";
-import Matrix from "../components/Matrix";
+import Description from '../components/Description';
 export default function Home() {
-  return (
-    <>
-      <main className="container mx-auto px-4 py-8">
-        <Description />
-        <Matrix />
-      </main>
-    </>
-  );
+    return (
+        <>
+            <main className="container mx-auto px-4 py-8">
+                <Description />
+            </main>
+        </>
+    );
 }
