bd01ad45ea6a9ac86467e542d96e54d89c22d2b7
ARed99 <chandyajin@gmail.com>
2024-12-03 11:24:00 +0530

    made description component support md

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -1,7 +1,26 @@
-'use client';
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 
-export default function Description() {
-    return (
-        <section className="mb-8 text-center relative"></section>
-    );
+interface DescriptionProps {
+    markdownContent: string;
 }
+
+const Description: React.FC<
+    DescriptionProps
+> = ({ markdownContent }) => {
+    return (
+        <section className="mb-8 text-center relative">
+            <div className="markdown">
+                <ReactMarkdown
+                    remarkPlugins={[
+                        remarkGfm,
+                    ]}>
+                    {markdownContent}
+                </ReactMarkdown>
+            </div>
+        </section>
+    );
+};
+
+export default Description;
