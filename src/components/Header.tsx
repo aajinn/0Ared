bbe23c5edf38568ddfaf01b86b88572a4b008467
ARed99 <chandyajin@gmail.com>
2025-05-05 21:12:36 +0530

    update X url

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,44 +1,38 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function Header() {
-    return (
-        <header className="py-4">
-            <div className="container mx-auto px-4">
-                <div className="flex flex-col items-center">
-                    <nav className="flex flex-wrap justify-center space-x-2 md:space-x-4">
-                        <Link
-                            href="https://github.com/aajinn"
-                            className="hover:text-gray-300 px-2 py-1 font-semibold underline">
-                            <span>
-                                Github
-                            </span>
-                        </Link>
-                        <Link
-                            href="https://x.com/ChandyAjin"
-                            className="hover:text-gray-300 px-2 py-1 font-semibold underline">
-                            <span>
-                                X
-                            </span>
-                        </Link>
-                    </nav>
-                </div>
-            </div>
-        </header>
-    );
+     return (
+          <header className="py-4">
+               <div className="container mx-auto px-4">
+                    <div className="flex flex-col items-center">
+                         <nav className="flex flex-wrap justify-center space-x-2 md:space-x-4">
+                              <Link
+                                   href="https://github.com/aajinn"
+                                   className="hover:text-gray-300 px-2 py-1 font-semibold underline"
+                              >
+                                   <span>Github</span>
+                              </Link>
+                              <Link
+                                   href="https://x.com/areddev"
+                                   className="hover:text-gray-300 px-2 py-1 font-semibold underline"
+                              >
+                                   <span>X</span>
+                              </Link>
+                         </nav>
+                    </div>
+               </div>
+          </header>
+     );
 }
 
-function NavLink({
-    href,
-    text,
-}: {
-    href: string;
-    text: string;
-}) {
-    return (
-        <Link
-            href={href}
-            className="hover:text-gray-300 px-2 py-1 font-semibold">
-            {text}
-        </Link>
-    );
+function NavLink({ href, text }: { href: string; text: string }) {
+     return (
+          <Link
+               href={href}
+               className="hover:text-gray-300 px-2 py-1 font-semibold"
+          >
+               {text}
+          </Link>
+     );
 }
+ChandyAjin;
